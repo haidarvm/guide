8973778e4dfb36f63f3e40aa4ceb62f9ccaec1a9
haidarvm <haidarvm@gmail.com>
2023-03-24 13:32:13 +0700

    upade git and pg dump restore

--- pg_dump_backup.sql
+++ pg_dump_backup.sql
@@ -1,10 +1,23 @@
 -- import db
-psql -U postgres -d invtcmit -a -f c:/invtcmit.sql
+psql -U postgres -d dbname -a -f c:/dbname.sql
 psql -U postgres -h localhost -d stack -a -f stack_pg.sql
 psql -U postgres -d stack -a -f stack_pg.sql
 
 -- export db
-pg_dump -U postgres -o -v -d invtcmit > C:/invtcmit.sql
+pg_dump dbname > db.sql
+pg_dump -U postgres -F c dbname > dbname.tar
+pg_dump -U postgres dangerousdb | gzip > dangerousdb.gz
+
+
+-- restore db
+-- if exists
+pg_restore -U postgres -Ft -d dbcooper < dbcooper.tar
+--- if not exist
+pg_restore -U postgres -Ft -C -d dbcooper < dbcooper.tar
+
+-- restore
+psql -f dbcopper.sql
+
 
 pg_dump -h localhost -U postgres --format plain --verbose --file wprg_posts.sql --table public.wprg_posts jabarnews
 
@@ -14,14 +27,12 @@ pg_dump -h localhost -U postgres  --verbose --file Posts.sql --table '"Posts"' s
 pg_dump -h localhost -U postgres -t '"Posts"' -t '"PostsId"' stack > stack_pg.sql
 
 
-pg_dump -h localhost -Fc test > /home/postgres/dump.sql
-
 
 -- drop db
-DROP DATABASE invtcmit;
+DROP DATABASE dbname;
 
 -- create db
-CREATE DATABASE invtcmit;
+CREATE DATABASE dbname;
 
 -- PGADMIN BACKUP
 pg_dump.exe --host localhost --port 5432 --username "postgres" --role "postgres" --no-password  --format tar --blobs --verbose --file "D:\backup\pg_gps5_11_15.backup" "gps_tcm"
