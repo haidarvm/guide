d67c57149d382006f42fe894105b11875ebf1e00
haidarvm <haidarvm@gmail.com>
2024-06-18 20:00:00 +0700

    Daily update

--- cronrsync.sh
+++ cronrsync.sh
@@ -1,1 +1,2 @@
-rsync -az /home/haidar/Documents/ra/ haidar@192.168.1.2:/home/haidar/backup/
+rsync -az /home/haidar/Documents/ haidar@192.168.1.2:/home/haidar/backup/
+rsync -az /home/haidar/Documents/ /home/haidar/backup/
